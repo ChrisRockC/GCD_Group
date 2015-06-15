4adcfb020d9670f0a7b45e6855ad43d41e0431a7
chenchaocc <ccerdong@sina.cn>
2015-06-16 00:19:37 +0800

    Initial Commit

---
+++ GCD_Group/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD_Group
+//
+//  Created by mac on 15/6/16.
+//  Copyright (c) 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD_Group/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCD_Group
+//
+//  Created by mac on 15/6/16.
+//  Copyright (c) 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
